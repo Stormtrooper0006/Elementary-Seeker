b93f79200ed29d6cb07deec607b7535be57de30c
Raden Akira Ziyad Asqar <akira123456789.aza@gmail.com>
2024-02-21 14:00:30 +0700

    About us text update

--- src/pages/about_us/AboutUs.jsx
+++ src/pages/about_us/AboutUs.jsx
@@ -8,9 +8,9 @@ function AboutUs() {
         <GroupRoundedIcon style={{ fontSize: "200px", color: "#6f42c1" }} />
         <h1 style={{ color: "#fff" }}>About Us</h1>
         <h2>
-          We aims to be able to help parents find private elementary schools
-          easier by providing list of private elementary schools that's
-          available and has been segmented by the schools province and regency.
+          We aim to help parents find private elementary schools more easily by
+          providing a list of available private elementary schools segmented by
+          province and regency.
         </h2>
       </div>
     </div>
